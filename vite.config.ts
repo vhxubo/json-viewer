48696dd5b34d85b9f20725d9c832bec72b1f5588
vhxubo <vhxubo@gmail.com>
2023-07-08 17:26:40 +0800

    chore: update vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,16 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/json-viewer/",
   plugins: [vue()],
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks: {
-  //         codemirror: [
-  //           "vue-codemirror6",
-  //           "codemirror",
-  //           // "@codemirror/autocomplete",
-  //           // "@codemirror/commands",
-  //           // "@codemirror/language",
-  //           // "@codemirror/lint",
-  //           // "@codemirror/search",
-  //           // "@codemirror/state",
-  //           // "@codemirror/view",
-  //         ],
-  //         "codemirror-lang": [
-  //           "@codemirror/lang-json",
-  //           // "@codemirror/lang-html",
-  //           // "@codemirror/lang-javascript",
-  //           // "@codemirror/lang-markdown",
-  //         ],
-  //       },
-  //     },
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          codemirror: ["vue-codemirror6", "codemirror"],
+          "codemirror-lang": ["@codemirror/lang-json"],
+        },
+      },
+    },
+  },
 });
